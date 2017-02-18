03886129cbf1060462aa717ff8a4c2c676ec1817
Bhavin Joshi <bajoshi@asu.edu>
2017-02-17 18:52:16 -0800

    some updates

--- slope_effects_density.py
+++ slope_effects_density.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import os
 import sys
+import time
+import datetime
 
 import matplotlib.pyplot as plt
 
@@ -15,6 +17,33 @@ if os.name == 'posix':
 elif os.name == 'nt':
     desktop = 'C:\Users\Heather\Desktop\\'
     slopedir = desktop + '\\slope-effects-density\\'
+
+def check_point_polygon(x, y, poly):
+    """
+    This function, which is the most important one, came from a 
+    solution online. I simply copy pasted it.
+
+    It checks whether the supplied coordinates of a point are within the area
+    enclosed by the supplied polygon vertices and returns True or False.
+    The algorithm is called the Ray Casting algorithm. 
+    """
+
+    n = len(poly)
+    inside = False
+
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+
+    return inside
 
 def plot_region(vert_x, vert_y, vert_x_cen, vert_y_cen, eff_rad, valid_in, valid_out,\
     region_name='orientale', save=False, with_craters=False):
@@ -49,6 +78,11 @@ def plot_region(vert_x, vert_y, vert_x_cen, vert_y_cen, eff_rad, valid_in, valid
 
 if __name__ == '__main__':
     
+    # Start time
+    start = time.time()
+    dt = datetime.datetime
+    print "Starting at --", dt.now()
+
     # read in catalogs
     vertices_cat = np.genfromtxt(slopedir + 'HF_vertices_m.csv', dtype=None, names=True, delimiter=',')
     craters_cat = np.genfromtxt(slopedir + 'CRATER_FullHF_m.csv', dtype=None, names=True, delimiter=',')
@@ -87,14 +121,72 @@ if __name__ == '__main__':
     valid_out = np.where(rad_vertices > eff_rad)[0]
     valid_in = np.where(rad_vertices < eff_rad)[0]
 
-    plot_region(vertices_x, vertices_y, vertices_x_center, vertices_y_center, eff_rad, valid_in, valid_out,\
-    region_name='orientale', save=True, with_craters=False)
+    #plot_region(vertices_x, vertices_y, vertices_x_center, vertices_y_center, eff_rad, valid_in, valid_out,\
+    #region_name='orientale', save=True, with_craters=False)
 
-    # ----------------    ---------------- # 
-    # define pixel array
-    pix_x = np.arange(-4e6, -1.5e6, 1e3)
-    pix_y = np.arange(-2e6, 0.5e6, 1e3)
+    # define inner and outer polygons
+    poly_outer = zip(vertices_x[valid_out], vertices_y[valid_out])
+    poly_inner = zip(vertices_x[valid_in], vertices_y[valid_in])
+    #print check_point_polygon(-3.5e6, -0.5e6, poly_outer)
+    #print check_point_polygon(-3.5e6, -0.5e6, poly_inner)
 
+    # ----------------  measure and populate pixel area function array  ---------------- # 
+    # read in pixel slope info
+    # this file also gives the x and y centers of pixels
+    slope_arr = np.load(home + '/Documents/plots_codes_for_heather/slope_effects_files/3km_slope_points.npy')
 
+    pix_x_cen_arr = slope_arr['pix_x_cen']
+    pix_y_cen_arr = slope_arr['pix_y_cen']
 
+    pix_centers = zip(pix_x_cen_arr, pix_y_cen_arr)
+
+    pix_area_arr = np.zeros(len(pix_centers))
+
+    # this can be optimized a LOT!!
+    # not all pixels have to be checked with both polygons
+    # in fact, most pixels don't need to be checked at all
+    for i in range(len(pix_centers)):
+
+        tl_x = pix_centers[i][0] - 5e2
+        tr_x = pix_centers[i][0] + 5e2
+        bl_x = pix_centers[i][0] - 5e2
+        br_x = pix_centers[i][0] + 5e2
+
+        tl_y = pix_centers[i][1] + 5e2
+        tr_y = pix_centers[i][1] + 5e2
+        bl_y = pix_centers[i][1] - 5e2
+        br_y = pix_centers[i][1] - 5e2
+
+        tl = [tl_x, tl_y]
+        tr = [tr_x, tr_y]
+        bl = [bl_x, bl_y]
+        br = [br_x, br_y]
+
+        pixel_corners = [tl, tr, bl, br]  # top and bottom, left and right
+        for corner in pixel_corners:
+
+            out_bool_tl = check_point_polygon(tl[0], tl[1], poly_outer)
+            in_bool_tl = check_point_polygon(tl[0], tl[1], poly_inner)
+
+            out_bool_tr = check_point_polygon(tr[0], tr[1], poly_outer)
+            in_bool_tr = check_point_polygon(tr[0], tr[1], poly_inner)            
+
+            out_bool_bl = check_point_polygon(bl[0], bl[1], poly_outer)
+            in_bool_bl = check_point_polygon(bl[0], bl[1], poly_inner)
+
+            out_bool_br = check_point_polygon(br[0], br[1], poly_outer)
+            in_bool_br = check_point_polygon(br[0], br[1], poly_inner)
+
+            out_bool = out_bool_tl and out_bool_tr and out_bool_bl and out_bool_br
+            in_bool = in_bool_tl and in_bool_tr and in_bool_bl and in_bool_br
+
+        if not out_bool:
+            pix_area_arr[i] = 0
+        if in_bool:
+            pix_area_arr[i] = 1
+
+        pix_area_arr[i] = pixel_area(x_cen, y_cen, poly)
+
+    # total run time
+    print "Total time taken --", time.time() - start, "seconds."
     sys.exit(0)
